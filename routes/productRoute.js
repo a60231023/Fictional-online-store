4a4413a6762e3488fe66b270517c8141df4dca11
a60231023 <ayushtiwari6061@gmail.com>
2023-05-07 12:25:59 +0530

    added admin update and delete product details

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,13 +5,29 @@ const {
   addProduct,
   getAllProduct,
   getOneProduct,
+  adminDeleteOneProduct,
+  adminUpdateOneProduct,
 } = require("../controllers/productController");
 const { isLoggedIn, customRole } = require("../middlewares/auth");
 
+//user -- get product details
 router.get("/testProduct", testProduct);
 router.get("/products", getAllProduct);
 router.get("/product/:id", getOneProduct);
 
+//admin -- product modify routes
 router.post("/admin/product/add", isLoggedIn, customRole("admin"), addProduct);
+router.put(
+  "/admin/product/update/:id",
+  isLoggedIn,
+  customRole("admin"),
+  adminUpdateOneProduct
+);
+router.delete(
+  "/admin/product/delete/:id",
+  isLoggedIn,
+  customRole("admin"),
+  adminDeleteOneProduct
+);
 
 module.exports = router;
