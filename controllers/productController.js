49f7c20ff84ff4d9e56efb5c3562bb985279df7b
a60231023 <ayushtiwari6061@gmail.com>
2023-05-07 11:29:51 +0530

    added get one product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -70,3 +70,16 @@ exports.getAllProduct = BigPromise(async (req, res) => {
     filteredProductLength,
   });
 });
+
+//get one product -- for all user
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {testProduct, addProduct, getAllProduct} = require("../controllers/productController");
+const {
+  testProduct,
+  addProduct,
+  getAllProduct,
+  getOneProduct,
+} = require("../controllers/productController");
 const { isLoggedIn, customRole } = require("../middlewares/auth");
-
 
 router.get("/testProduct", testProduct);
 router.get("/products", getAllProduct);
+router.get("/product/:id", getOneProduct);
 
 router.post("/admin/product/add", isLoggedIn, customRole("admin"), addProduct);
-
 
 module.exports = router;
